347fdf14ffefaa6f834983bd365ae62599054c80
Hephzibah <hephzibahijeje89@gmail.com>
2024-03-06 07:53:18 -0500

    files

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'success' };
+function handleResponseFromAPI(promise) {
   return promise
-    .then(() => obj)
-    .catch(() => new Error())
-    .finally(() => { console.log('Got a response from the API'); });
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error();
+    });
 }
