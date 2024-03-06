77dcf39ee75ba821bc7251214235c371c012a928
Hephzibah <hephzibahijeje89@gmail.com>
2024-03-06 07:44:24 -0500

    task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,16 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    })
+async function handleProfileSignup() {
+  try {
+    const [userData, photoData] = await Promise.all([
+      createUser(),
+      uploadPhoto()
+    ]);
 
-    .catch(() => console.log('Signup system offline'));
+    console.log(`body ${userData.firstName} ${userData.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
+
+export default handleProfileSignup;
