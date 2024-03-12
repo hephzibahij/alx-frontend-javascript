33af64fdb11a3003883c4398bdc05175195047dd
Hephzibah <hephzibahijeje89@gmail.com>
2024-03-12 18:44:19 -0400

    task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,11 @@
 export const weakMap = new WeakMap();
-
 export function queryAPI(endpoint) {
-  let called = weakMap.get(endpoint) || 0;
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
 
-  called += 1;
-
-  weakMap.set(endpoint, called);
-
-  if (called >= 5) {
-    throw Error('Endpoint load is high');
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, count);
   }
-
-  return called;
+}
